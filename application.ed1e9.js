eae5d6c1f661deb626df5987a645b41c6f10d5e5
kyrylokalienich <155952445+kyrylokalienich@users.noreply.github.com>
2025-05-08 16:50:26 +0300

    3

--- application.ed1e9.js
+++ application.ed1e9.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.76939.json';
+          this.settingsPath = 'src/settings.777fb.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.79560.js
+++ index.79560.js
@@ -1,4 +1,4 @@
-System.register(["./application.32f6d.js"], function (_export, _context) {
+System.register(["./application.ed1e9.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
