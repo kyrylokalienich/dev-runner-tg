bb8a00b26ff421d19c7c6f1c43911f3c3248a5c6
kyrylokalienich <155952445+kyrylokalienich@users.noreply.github.com>
2025-05-08 16:39:13 +0300

    aa

--- index.38ea3.js
+++ index.38ea3.js
@@ -1,4 +1,4 @@
-System.register(["./application.352f2.js"], function (_export, _context) {
+System.register(["./application.32f6d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
